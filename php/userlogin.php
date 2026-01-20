8167b4ba2115cafa3a421c789f6357b110da2131
Pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2026-01-20 13:02:19 -0500

    Cleaned up code for fetching row with same email and password.

--- php/userlogin.php
+++ php/userlogin.php
@@ -2,14 +2,39 @@
     include "credentials.php";
     $dbuser = $dbusername;
     $password = $dbuserpass;
-    /*Create PDO object and instantiate connection to database*/
 
+    /*Create PDO object and instantiate connection to database*/
     try {
         $conn = new PDO("mysql:host=webapp-database-1;dbname=myapp", $dbuser, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Success";
 
     } catch (PDOException $e) {
         echo "Failed to connect: " . $e;
     }
-    
+
+    /*Create prepared statement, query server to check email and password*/
+    $useremail = $_POST['email'];
+    $userpass = $_POST['password'];
+
+    verifyEmailPassword($conn, $useremail, $userpass);
+
+    /* Function which queries MySQL server for username and password */
+    function verifyEmailPassword($conn, $email, $pass){
+        $sqlcheck = "SELECT name FROM user
+        WHERE email = :email AND password_hash = :pass;";
+
+        $hashed_pass = password_verify($pass, PASSWORD_DEFAULT);
+        $stmt = $conn->prepare($sqlcheck);
+
+        $data = [
+            'email' => $email,
+            'pass' => $hashed_pass
+        ];
+
+        $result = $stmt->execute($data);
+        echo $result;
+        
+    }
+
 ?>
