2e41dd29e477c22b8bb26aeec6c4a63b3953b78d
Pythonic <124443031+Pythonic2023@users.noreply.github.com>
2026-02-04 13:06:24 -0500

    Styled readnote.php.

--- src/readnote.php
+++ src/readnote.php
@@ -1,10 +1,16 @@
 <?php 
     session_start();
+    
     require '../php/retrievenote.php';
+    $pageCss = 'readnote.css';
+    include 'base.php';
 
 ?>
 
 
 <main>
-    <?php echo $notecontents['content']; ?>
+    <div id="note-content">
+        <div class="note"> <?php echo $notecontents['title']; ?> </div>
+        <div class="note"> <?php echo $notecontents['content']; ?> </div>
+    </div>
 </main>
